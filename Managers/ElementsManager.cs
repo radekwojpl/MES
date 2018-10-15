2b166cb5a42c5da04b69e28f45b39408151141c8
radekwojpl <radekbajor27@gmail.com>
2018-10-15 23:43:55 +0200

    .

---
+++ Managers/ElementsManager.cs
@@ -0,0 +1,18 @@
+﻿using MES_App.BasicStruct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_App.Managers
+{
+    public class ElementsManager
+    {
+      
+        private static List<Element> BuildElements()
+        {
+            return null;
+        }
+    }
+}
